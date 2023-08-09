8159fa7acd3b2f48eefaba494e8a9daea320fa8d
AndrewRomanyshyn <andrewtheere@gmail.com>
2023-08-09 14:13:42 +0200

    update

--- database/weather_database.py
+++ database/weather_database.py
@@ -1,26 +1,22 @@
 import psycopg2
+
 from config.config import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
 
-def truncate_table():
-    try:
-        connection = psycopg2.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME
-        )
+def database_connect():
+    with psycopg2.connect(
+        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME
+    ) as connection:
         connection.autocommit = True
-        cursor = connection.cursor()
+        return connection
 
-        cursor.execute(
-            """TRUNCATE TABLE weather_info RESTART IDENTITY"""
-        )
-        print('[INFO] Truncated successful')
+
+def truncate_table():
+    connection = database_connect()
+    try:
+        connection.autocommit = True
+        with connection.cursor() as cursor:
+            cursor.execute("""TRUNCATE TABLE weather_info RESTART IDENTITY""")
+            print("[INFO] Truncated successful")
     except Exception as ex:
-        print(f'[ERROR] PostgreSQL gone wrong {ex}')
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-
+        print(f"[ERROR] PostgreSQL gone wrong {ex}")
